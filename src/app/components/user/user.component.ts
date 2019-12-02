bfea3a33c37125a80e0bc53ec736e4fe322bc86f
HugoBoxFr <hugo.prea@sfr.fr>
2019-12-02 11:41:54 +0100

    exo2

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, FormGroup, FormBuilder } from '@angular/forms';
 import { User } from 'src/app/shared/user';
 
 @Component({
@@ -9,31 +9,31 @@ import { User } from 'src/app/shared/user';
 })
 export class UserComponent implements OnInit {
   title: string = "Reactive Form";
-  username = new FormControl('');
-  email = new FormControl('');
-  password = new FormControl('');
-  street = new FormControl('');
-  postal = new FormControl('');
-  city = new FormControl('');
+
+  userForm = this.fb.group({
+    username: [''],
+    credentials: this.fb.group({
+      email: [''],
+      password: ['']
+    }),
+    address: this.fb.group({
+        street: [''],
+        postal: [''],
+        city: ['']
+    })
+  });
+
   user: User = new User;
   users: User[] = [];
   userDisplay: boolean = false;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
   }
 
-  signIn() {
-      this.user.username = this.username.value;
-      this.user.email = this.email.value;
-      this.user.password = this.password.value;
-      this.user.street = this.street.value;
-      this.user.postal = this.postal.value;
-      this.user.city = this.city.value;
-      this.userDisplay = true;
-      this.users.push(this.user);
-      this.user = new User;2
+  onSubmit() {
+    console.log(this.userForm.value);
   }
 
 }
